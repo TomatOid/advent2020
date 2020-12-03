282b51e955b3f13b7a31edf017bc5ae54cece899
Connor <connormc757@gmail.com>
2020-12-03 09:31:32 -0500

    file loading, new function

--- day_3/main.zig
+++ day_3/main.zig
@@ -1,9 +1,11 @@
 const std = @import("std");
+const fs = std.fs;
+const stdout = std.io.getStdOut().writer();
 
 const Map = struct {
     width: usize,
     height: usize,
-    data: []const u8,
+    data: []u8,
 
     pub fn getCharAt(self: Map, x: usize, y: usize) ?u8 {
         if (y >= self.height) {
@@ -13,13 +15,42 @@ const Map = struct {
     }
 };
 
-test "map" {
-    var map: Map = .{ .width = 11, .height = 11, .data = undefined };
-    map.data = "..##.......#...#...#...#....#..#...#.#...#.#.#...##..#...#.##......#.#.#....#.#........##.##...#...#...##....#.#..#...#.#";
-    var y: usize = 0;
+pub fn countTrees(mountain: Map, delta_x: usize, delta_y: usize) usize {
     var trees_count: usize = 0;
-    while (map.getCharAt(3 * y, y)) |obsticle| : (y += 1) {
+    var y: usize = 0;
+    var x: usize = 0;
+    while (mountain.getCharAt(x, y)) |obsticle| : (y += delta_y) {
         trees_count += @boolToInt(obsticle == '#');
+        x += delta_x;
     }
-    std.debug.assert(trees_count == 7);
+    return trees_count;
+}
+
+test "map" {
+    const map_string = "..##.......#...#...#...#....#..#...#.#...#.#.#...##..#...#.##......#.#.#....#.#........##.##...#...#...##....#.#..#...#.#";
+    var map_buffer: [map_string.len]u8 = undefined;
+    std.mem.copy(u8, map_buffer[0..], map_string);
+    var map: Map = .{ .width = 11, .height = 11, .data = map_buffer[0..] };
+    std.debug.assert(countTrees(map, 3, 1) == 7);
+}
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{ .safety = false }){};
+    const allocator = &gpa.allocator;
+    var cwd = fs.cwd();
+    var file = try cwd.openFile("map.txt", .{ .read = true, .write = false });
+    var mountain: Map = .{
+        .width = 0,
+        .height = 0,
+        .data = try allocator.alloc(u8, (try file.getEndPos()) + 1),
+    };
+    var file_slice: []u8 = mountain.data[0..];
+    var file_index: usize = 0;
+    while (try file.reader().readUntilDelimiterOrEof(file_slice, '\n')) |line| {
+        file_index += line.len;
+        file_slice = mountain.data[file_index..];
+        mountain.width = line.len;
+        mountain.height += 1;
+    }
+    try stdout.print("{}\n", .{trees_count});
 }
