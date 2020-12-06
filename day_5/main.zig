cb4d7ec718863653197d7bd6aba07842f8a3e7f7
Connor <connormc757@gmail.com>
2020-12-05 20:04:37 -0500

    tests passing

---
+++ day_5/main.zig
@@ -0,0 +1,17 @@
+const std = @import("std");
+
+pub fn seatToRowCol(seat_str: []const u8) [2]u32 {
+    var seat_num: u32 = 0;
+    for (seat_str) |char| {
+        seat_num <<= 1;
+        seat_num |= @boolToInt(char == 'B' or char == 'R');
+    }
+    return [2]u32{ seat_num >> 3, seat_num & 0x7 };
+}
+
+test "rows / columns" {
+    var pos = seatToRowCol("BFFFBBFRRR");
+    std.testing.expect(pos[0] == 70 or pos[1] == 7);
+    pos = seatToRowCol("FFFBBBFRRR");
+    std.testing.expect(pos[0] == 14 or pos[1] == 119);
+}
