665e003f199697b23c3ebb624f681191b60bef61
Connor <connormc757@gmail.com>
2020-12-05 20:27:16 -0500

    heck yea part 1

--- day_5/main.zig
+++ day_5/main.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const fs = std.fs;
 
 pub fn seatToRowCol(seat_str: []const u8) [2]u32 {
     var seat_num: u32 = 0;
@@ -9,6 +10,15 @@ pub fn seatToRowCol(seat_str: []const u8) [2]u32 {
     return [2]u32{ seat_num >> 3, seat_num & 0x7 };
 }
 
+pub fn seatToInt(seat_str: []const u8) u32 {
+    var seat_num: u32 = 0;
+    for (seat_str) |char| {
+        seat_num <<= 1;
+        seat_num |= @boolToInt(char == 'B' or char == 'R');
+    }
+    return seat_num;
+}
+
 test "rows / columns" {
     var pos = seatToRowCol("BFFFBBFRRR");
     std.testing.expect(pos[0] == 70 and pos[1] == 7);
@@ -16,4 +26,16 @@ test "rows / columns" {
     std.testing.expect(pos[0] == 14 and pos[1] == 7);
 }
 
-//pub fn main() !void {
+pub fn main() !void {
+    var cwd = fs.cwd();
+    var file = (try cwd.openFile("passes.txt", .{ .read = true, .write = false })).reader();
+    var max_id: u32 = 0;
+    var file_buffer: [256]u8 = undefined;
+    while (try file.readUntilDelimiterOrEof(file_buffer[0..], '\n')) |line| {
+        var seat_id = seatToInt(line);
+        if (seat_id > max_id) {
+            max_id = seat_id;
+        }
+    }
+    try std.io.getStdOut().writer().print("{}\n", .{max_id});
+}
