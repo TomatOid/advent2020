03831e5f0c5e7b5d734877a50c7d9ef15f53cabe
Connor <connormc757@gmail.com>
2020-12-05 20:58:14 -0500

    day 5 done

--- day_5/main.zig
+++ day_5/main.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const fs = std.fs;
+const stdout = std.io.getStdOut().writer();
 
 pub fn seatToRowCol(seat_str: []const u8) [2]u32 {
     var seat_num: u32 = 0;
@@ -30,12 +31,21 @@ pub fn main() !void {
     var cwd = fs.cwd();
     var file = (try cwd.openFile("passes.txt", .{ .read = true, .write = false })).reader();
     var max_id: u32 = 0;
-    var file_buffer: [256]u8 = undefined;
+    var file_buffer: [12]u8 = undefined;
+    var seat_ids: [837]u32 = undefined;
+    var seat_counter: usize = 0;
     while (try file.readUntilDelimiterOrEof(file_buffer[0..], '\n')) |line| {
         var seat_id = seatToInt(line);
-        if (seat_id > max_id) {
-            max_id = seat_id;
+        if (seat_counter < seat_ids.len) {
+            seat_ids[seat_counter] = seat_id;
+            seat_counter += 1;
+        } else return error.FileTooLong;
+    }
+    std.sort.sort(u32, seat_ids[0..seat_counter], {}, comptime std.sort.asc(u32));
+    var i: usize = 0;
+    while (i < seat_counter - 1) : (i += 1) {
+        if (seat_ids[i + 1] - seat_ids[i] > 1) {
+            try stdout.print("{} {}\n", .{ seat_ids[i], seat_ids[i + 1] });
         }
     }
-    try std.io.getStdOut().writer().print("{}\n", .{max_id});
 }
