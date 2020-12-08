6b97f63b7cd4f96dd2fad50a6f842dc7e59125a1
Connor <connormc757@gmail.com>
2020-12-08 15:26:45 -0500

    day 7 done

--- day_7/main.zig
+++ day_7/main.zig
@@ -3,16 +3,17 @@ usingnamespace @import("../utils.zig");
 const stdout = std.io.getStdOut().writer();
 
 const TreeNode = struct {
-    const Node = std.SinglyLinkedList(*TreeNode).Node;
+    const Node = std.SinglyLinkedList(struct { branch: *TreeNode, count: u8 }).Node;
     children: ?*Node = null,
     name: []const u8,
     visited: bool,
 
-    pub fn addChild(self: *TreeNode, child: *TreeNode, allocator: *std.mem.Allocator) !void {
+    pub fn addChild(self: *TreeNode, child: *TreeNode, child_count: u8, allocator: *std.mem.Allocator) !void {
         var child_node = try allocator.create(Node);
-        child_node.data = child;
-        if (self.children) |sub_planets| {
-            sub_planets.insertAfter(child_node);
+        child_node.data.branch = child;
+        child_node.data.count = child_count;
+        if (self.children) |sub_bags| {
+            sub_bags.insertAfter(child_node);
         } else {
             child_node.next = null;
             self.children = child_node;
@@ -31,6 +32,7 @@ const TreeNode = struct {
     pub fn init(self: *TreeNode, name: []const u8) *TreeNode {
         self.children = null;
         self.visited = false;
+        self.name = name;
         return self;
     }
 
@@ -39,10 +41,23 @@ const TreeNode = struct {
         var current_node = self.children;
         while (current_node) |value| {
             current_node = value.next;
-            if (value.data.visited) continue;
-            count += 1 + value.data.countDescendents();
+            if (value.data.branch.visited) continue;
+            count += 1 + value.data.branch.countDescendents();
         }
         self.visited = true;
+        return count;
+    }
+
+    pub fn countDescendentsAmount(self: *TreeNode) usize {
+        var count: usize = 0;
+        var current_node = self.children;
+        while (current_node) |value| {
+            current_node = value.next;
+            if (value.data.branch.visited) continue;
+            count += @intCast(u64, value.data.count) * (1 + value.data.branch.countDescendentsAmount());
+        }
+        stdout.print("{} {}\n", .{ self.name, count }) catch |_| {};
+        //self.visited = true;
         return count;
     }
 };
@@ -82,14 +97,14 @@ pub fn buildTree(file: []const u8, bags_map: *std.StringHashMap(*TreeNode), allo
         while (comma_iterator.next()) |child_bag| {
             var child_string = stripIrrelevant(child_bag);
             var child: *TreeNode = undefined;
-            if (std.mem.eql(u8, child_string, "no other bags")) break;
+            if (std.mem.eql(u8, child_string, "no other bag")) break;
             if (bags_map.get(child_string)) |child_value| {
                 child = child_value;
             } else {
                 child = (try allocator.create(TreeNode)).init(child_string);
                 try bags_map.put(child_string, child);
             }
-            try child.addChild(parent, allocator);
+            try child.addChild(parent, 1, allocator);
         }
     }
 }
@@ -119,6 +134,60 @@ test "build tree" {
     std.testing.expect(gold_node.countDescendents() == 4);
 }
 
+pub fn buildTreeBackwards(file: []const u8, bags_map: *std.StringHashMap(*TreeNode), allocator: *std.mem.Allocator) !void {
+    var line_iterator = getDelimIterator("\n", file);
+    while (line_iterator.next()) |line| {
+        var parent_child = splitString(" contain ", line) orelse return error.NoContain;
+        var parent_string = stripIrrelevant(parent_child[0]);
+        var parent: *TreeNode = undefined;
+        if (bags_map.get(parent_string)) |parent_value| {
+            parent = parent_value;
+        } else {
+            parent = (try allocator.create(TreeNode)).init(parent_string);
+            try bags_map.put(parent_string, parent);
+        }
+        var comma_iterator = getDelimIterator(", ", parent_child[1][0 .. parent_child[1].len - 1]);
+        while (comma_iterator.next()) |child_bag| {
+            if (std.mem.eql(u8, child_bag, "no other bags")) break;
+            var child_string = stripIrrelevant(child_bag);
+            var child_count = child_bag[0] - '0';
+            var child: *TreeNode = undefined;
+            if (bags_map.get(child_string)) |child_value| {
+                child = child_value;
+            } else {
+                child = (try allocator.create(TreeNode)).init(child_string);
+                try bags_map.put(child_string, child);
+            }
+
+            try parent.addChild(child, child_count, allocator);
+        }
+    }
+}
+
+test "count nested" {
+    var file =
+        \\shiny gold bags contain 2 dark red bags, 5 bright pink bags.
+        \\dark red bags contain 2 dark orange bags.
+        \\dark orange bags contain 2 dark yellow bags.
+        \\striped bags contain 3 shiny gold bags.
+        \\dark yellow bags contain 2 dark green bags.
+        \\dark green bags contain 2 dark blue bags.
+        \\dark blue bags contain 2 dark violet bags.
+        \\dark violet bags contain no other bags.
+    ;
+    var bags_map = std.StringHashMap(*TreeNode).init(std.testing.allocator);
+    defer bags_map.deinit();
+    defer {
+        var iterator = bags_map.iterator();
+        while (iterator.next()) |kv| {
+            kv.value.freeSelf(std.testing.allocator);
+        }
+    }
+    try buildTreeBackwards(file, &bags_map, std.testing.allocator);
+    var gold_node = bags_map.get("shiny gold bag") orelse unreachable;
+    std.testing.expect(gold_node.countDescendentsAmount() == 131);
+}
+
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = &gpa.allocator;
@@ -138,7 +207,7 @@ pub fn main() !void {
         }
     }
 
-    try buildTree(file_buffer[0 .. file_length - 1], &bags_map, allocator);
+    try buildTreeBackwards(file_buffer[0 .. file_length - 1], &bags_map, allocator);
     var gold_node = bags_map.get("shiny gold bag") orelse return error.NoShinyGold;
-    try stdout.print("{}\n", .{gold_node.countDescendents()});
+    try stdout.print("{}\n", .{gold_node.countDescendentsAmount()});
 }
